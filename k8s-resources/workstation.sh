85ee594de2d784588ef8dcc1ab10ec3dcda96169
pentadushyanth <pentadushyanth@gmail.com>
2026-02-24 18:30:40 +0530

    kubernetes files

--- k8s-resources/workstation.sh
+++ k8s-resources/workstation.sh
@@ -10,11 +10,15 @@ systemctl enable docker
 usermod -aG docker ec2-user
 
 cd ~
-sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-sudo ARCH=amd64
-sudo PLATFORM=$(uname -s)_$ARCH
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+
+
+
